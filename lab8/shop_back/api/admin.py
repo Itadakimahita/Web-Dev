1fdf36a8341be523be6a490d36c7ee7d794abc89
Itadakimahita <kadirov.dias@mail.ru>
2025-03-26 01:01:08 +0500

    lab8

---
+++ lab8/shop_back/api/admin.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+from .models import Product, Category
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name') 
+    search_fields = ('name',)      
+    ordering = ('name',)           
+
+# Register Product model
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'count', 'is_active', 'category')  
+    list_filter = ('is_active', 'category')
+    search_fields = ('name', 'description')  
+    list_editable = ('price', 'count', 'is_active')  
+    ordering = ('name',) 
+    
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'category')
+        }),
+        ('Details', {
+            'fields': ('price', 'description', 'count', 'is_active')
+        }),
+    )
+
+# admin.site.register(Category)
+# admin.site.register(Product)
